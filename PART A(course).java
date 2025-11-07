860f6b93be403663276b733fa61d5519bdbb8e48
Rishi <23bcs14134@cuchd.in>
2025-11-07 22:40:55 +0530

    Create PART A(course).java

---
+++ PART A(course).java
@@ -0,0 +1,15 @@
+package com.example;
+
+// Author: RISHI KUMAR | UID: 23BCS14134
+
+public class Course {
+    private String courseName;
+
+    public Course() {
+        this.courseName = "Spring Framework";
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
